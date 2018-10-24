2fe864f2637841df191e92f481762ff25964d88c
yagiri000 <yohakuari@gmail.com>
2018-10-24 22:35:19 +0900

    時間のテストを追加

--- ChronoTest/Main.cpp
+++ ChronoTest/Main.cpp
@@ -1,34 +1,35 @@
 #include <iostream>
 #include <functional>
 #include <chrono>
-
-bool is_pow2(unsigned num) {
-	int count = 0;
-	for (; num > 0; num >>= 1) {
-		if ((num & 1) == 1) {
-			if (count > 0) {
-				return false;
-			}
-			else {
-				count++;
-			}
-		}
-	}
-	return true;
-}
-bool is_pow2_closed(unsigned num) {
-	return ((num != 0) && !(num & (num - 1)));
-}
-
+#include <vector>
+#include <array>
 
 int main()
 {
-	constexpr int N = 1000 * 1000;
+	constexpr size_t N = 10000;
+	constexpr size_t M = 10000;
+	std::vector<int> arr;
+	arr.resize(N);
+	std::vector<int> arr2;
+	arr2.resize(M);
+	std::vector<int> result;
+	result.resize(N*M);
 
 	{
+		for (size_t i = 0; i < N; i++) {
+			arr[i] = rand();
+		}
+		for (size_t i = 0; i < M; i++) {
+			arr2[i] = rand();
+		}
 		auto start = std::chrono::system_clock::now();      // 計測スタート時刻を保存
-		for (int i = 0; i < N; i++) {
-			is_pow2(256);
+
+		size_t count = 0;
+		for (size_t i = 0; i < N; i++) {
+			for (size_t j = 0; j < M; j++) {
+				result[count] = arr[i] + arr2[j];
+				count++;
+			}
 		}
 		auto end = std::chrono::system_clock::now();       // 計測終了時刻を保存
 		auto msec = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
@@ -37,18 +38,8 @@ int main()
 
 	}
 
-	{
-		auto start = std::chrono::system_clock::now();      // 計測スタート時刻を保存
-		for (int i = 0; i < N; i++) {
-			is_pow2_closed(256);
-		}
-		auto end = std::chrono::system_clock::now();       // 計測終了時刻を保存
-		auto msec = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-		// 要した時間をミリ秒（1/1000秒）に変換して表示
-		std::cout << msec << " milli sec \n";
 
-	}
-
+	std::cout << " milli sec \n";
 
 	return 0;
 }
